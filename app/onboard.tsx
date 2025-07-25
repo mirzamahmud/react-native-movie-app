18f4f2a0acbde78c17efbbc26436bfea46a45207
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-07-26 01:02:34 +0600

    create a onboard route

---
+++ app/onboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Onboard() {
+    return (
+        <View>
+            <Text>Onboard Screen</Text>
+        </View>
+    );
+}
+
+const onboard_styles = StyleSheet.create({})
